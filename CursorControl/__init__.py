4ba6f6d0038a55cac1795c62845a65523b051b77
Sean Chen <00schen@berkeley.edu>
2020-03-05 13:20:28 -0800

    removed cursorcontrolv1

--- CursorControl/__init__.py
+++ CursorControl/__init__.py
@@ -1,9 +1,6 @@
 from gym.envs.registration import register
 import gym
 
-# if 'cursorcontrol-v1' in gym.envs.registry.env_specs:
-del gym.envs.registry.env_specs['cursorcontrol-v1']
-
 register(id='cursorcontrol-v1',
         entry_point='cursorcontrolv1.envs:CursorControl',
         )
--- cursorcontrolv1/__init__.py
+++
@@ -1,5 +0,0 @@
-from gym.envs.registration import register
-
-register(id='cursorcontrol-v1',
-        entry_point='cursorcontrolv1.envs:CursorControl',
-        )
