7d2bd7e26694d93fa7113ddacdfca2be927dc434
Sean Chen <00schen@berkeley.edu>
2020-03-05 13:13:15 -0800

    fixed gym registry

--- CursorControl/__init__.py
+++ CursorControl/__init__.py
@@ -1,8 +1,8 @@
 from gym.envs.registration import register
 import gym
 
-if 'cursorcontrol-v1' in gym.envs.registry.env_specs:
-        del gym.envs.registry.env_specs['cursorcontrol-v1']
+# if 'cursorcontrol-v1' in gym.envs.registry.env_specs:
+del gym.envs.registry.env_specs['cursorcontrol-v1']
 
 register(id='cursorcontrol-v1',
         entry_point='cursorcontrolv1.envs:CursorControl',
