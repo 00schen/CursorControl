da015fc6ea206afc353ee4e8385c4d3b77e4ac08
Sean Chen <00schen@berkeley.edu>
2020-03-04 17:55:59 -0800

    new renderer

--- cursorcontrolv1/envs/env.py
+++ cursorcontrolv1/envs/env.py
@@ -83,14 +83,8 @@ class CursorControl(gym.Env):
     self.screen.fill(pygame.color.THECOLORS["white"])
     pygame.draw.circle(self.screen, (10,10,10,0), self.pos.astype(int)*500, 20)
     pygame.draw.circle(self.screen, (76,187,23,0), self.goal.astype(int)*500, 5)
-    pygame.draw.line(self.screen, (200, 10, 10), self.pos.astype(int)*500,\
-      self.pos.astype(int)*500+\
-        (np.array([np.cos(self.prev_action[0]),np.sin(self.prev_action[0])])*self.prev_action[1]*500).astype(int),\
-      width=2)
-    pygame.draw.line(self.screen, (10, 10, 200), self.pos*500,\
-      self.pos.astype(int)*500+\
-        (np.array([np.cos(self.prev_obs[3]),np.sin(self.prev_obs[3])])*self.prev_obs[4]*500).astype(int),\
-      width=2)
+    pygame.draw.line(self.screen, (200, 10, 10), self.pos.astype(int)*500,self.pos.astype(int)*500+(np.array([np.cos(self.prev_action[0]),np.sin(self.prev_action[0])])*self.prev_action[1]*500).astype(int),width=2)
+    pygame.draw.line(self.screen, (10, 10, 200), self.pos*500,self.pos.astype(int)*500+(np.array([np.cos(self.prev_obs[3]),np.sin(self.prev_obs[3])])*self.prev_obs[4]*500).astype(int),width=2)
 
 # simulate user with optimal intended actions that go directly to the goal
 def make_oracle_policy(goal,noise_sd=1):
