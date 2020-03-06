7e5b32bbc31377220c9ac90a6a5c072bd78bb26c
Sean Chen <00schen@berkeley.edu>
2020-03-05 22:44:12 -0800

    Added path saving and graphing

--- train.py
+++ train.py
@@ -1,20 +1,40 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import gym
 import tensorflow as tf
 import time
+import os
 
 import CursorControl
 
 from stable_baselines.sac import MlpPolicy
 from stable_baselines import SAC
+from stable_baselines import results_plotter
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines.common.callbacks import EvalCallback
+
+
+time_now = time.strftime('%Y-%m-%d-%H-%M', time.localtime())
+best_model_save_path = "sac_best_%s" % time_now
+log_path = "sac_%s" % time_now
+os.makedirs(best_model_save_path, exist_ok=True)
+os.makedirs(log_path, exist_ok=True)
 
 env = gym.make('cursorcontrol-v1')
+env = Monitor(env, log_path)
+eval_env = gym.make('cursorcontrol-v1')
 
 model = SAC(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=10000)
+callback = EvalCallback(eval_env, best_model_save_path=best_model_save_path, log_path=log_path)
+
+time_steps = int(1e6)
+
+model.learn(total_timesteps=time_steps,callback=callback)
 print("Training Done")
-time_now = time.strftime('%Y-%m-%d-%H-%M', time.localtime())
-model.save("sac_%s" % time_now)
+
+results_plotter.plot_results([log_path], time_steps, results_plotter.X_TIMESTEPS, "SAC CursorControl")
+plt.show()
 
 obs = env.reset()
 for i in range(100):
