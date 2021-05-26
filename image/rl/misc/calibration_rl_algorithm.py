updated light switch env (random distance from wall, variable switches, calibration/online distribution shift), keyboard policy (for debugging), adjusted reward fn, fixed KL div calculation

image/rl/misc/calibration_rl_algorithm.py
@@ -15,6 +15,7 @@ class BatchRLAlgorithm(TorchBatchRLAlgorithm, metaclass=abc.ABCMeta):
         trajs_per_index,
         calibration_buffer: ReplayBuffer,
         pretrain_steps=100,
+        max_failures=10,
         **kwargs,
     ):
         super().__init__(
@@ -22,11 +23,12 @@ class BatchRLAlgorithm(TorchBatchRLAlgorithm, metaclass=abc.ABCMeta):
         )
         self.calibration_data_collector = calibration_data_collector
         if calibration_indices is None:
-            calibration_indices = np.arange(self.expl_env.base_env.num_targets)
+            calibration_indices = self.expl_env.base_env.target_indices
         self.calibration_indices = calibration_indices
         self.trajs_per_index = trajs_per_index
         self.calibration_buffer = calibration_buffer
         self.pretrain_steps = pretrain_steps
+        self.max_failures = max_failures
 
     def _sample_and_train(self, steps, buffer):
         self.training_mode(True)
@@ -58,9 +60,10 @@ class BatchRLAlgorithm(TorchBatchRLAlgorithm, metaclass=abc.ABCMeta):
         self.eval_env.per_step = True
 
         # calibrate
-        for index in self.calibration_indices:
-            self.expl_env.new_goal(index)
-            for _ in range(self.trajs_per_index):
+        # self.expl_env.base_env.calibrate_mode(True)
+        for _ in range(self.trajs_per_index):
+            for index in self.calibration_indices:
+                self.expl_env.new_goal(index)
                 calibration_paths = self.calibration_data_collector.collect_new_paths(
                     self.max_path_length,
                     1,
@@ -70,6 +73,9 @@ class BatchRLAlgorithm(TorchBatchRLAlgorithm, metaclass=abc.ABCMeta):
                 self.calibration_buffer.add_paths(calibration_paths)
         gt.stamp('pretrain exploring', unique=False)
         self._sample_and_train(self.pretrain_steps, self.calibration_buffer)
+
+        # self.expl_env.base_env.calibrate_mode(False)
+        # self.eval_env.base_env.calibrate_mode(False)
 
         for epoch in gt.timed_for(
                 range(self._start_epoch, self.num_epochs),
@@ -89,6 +95,8 @@ class BatchRLAlgorithm(TorchBatchRLAlgorithm, metaclass=abc.ABCMeta):
                     success = False
                     failed_paths = []
                     successful_paths = []
+
+                    # switch positions do not change
                     while not success:
                         new_expl_paths = self.expl_data_collector.collect_new_paths(
                             self.max_path_length,
@@ -102,10 +110,11 @@ class BatchRLAlgorithm(TorchBatchRLAlgorithm, metaclass=abc.ABCMeta):
                                 success = True
                             else:
                                 failed_paths.append(path)
-                        if len(failed_paths) >= 10:
+                        if len(failed_paths) >= self.max_failures:
                             break
 
                     # no actual relabeling right now, since goals for all paths should be same
+                    # only add to paths to buffer if successful
                     if len(successful_paths):
                         self.replay_buffer.add_paths(successful_paths + failed_paths)
                         gt.stamp('data storing', unique=False)

image/rl/misc/env_wrapper.py
@@ -268,12 +268,12 @@ class static_gaze:
 
 	def _step(self,obs,r,done,info):
 		if self.per_step:
-			self.static_gaze = self.sample_gaze(info['unique_index'])
+			self.static_gaze = self.sample_gaze(self.master_env.base_env.target_indices.index(info['unique_index']))
 		obs['gaze_features'] = self.static_gaze
 		return obs,r,done,info
 
 	def _reset(self,obs,info=None):
-		index = self.master_env.base_env.unique_index
+		index = self.master_env.base_env.target_indices.index(self.master_env.base_env.unique_index)
 		obs['gaze_features'] = self.static_gaze = self.sample_gaze(index)
 		return obs
 
@@ -321,6 +321,8 @@ class reward:
 		self.reward_type = config['reward_type']
 		if self.master_env.env_name == 'Bottle' and self.reward_type == 'sparse':
 			self.reward_type = 'part_sparse'
+		self.reward_temp = config['reward_temp']
+		self.reward_offset = config['reward_offset']
 
 	def _step(self, obs, r, done, info):
 		if self.reward_type == 'custom':
@@ -334,8 +336,10 @@ class reward:
 		elif self.reward_type == 'custom_switch':
 			r = 0
 			if not info['task_success']:
-				target_pos = np.array(info['switch_pos'])[info['target_index']]
-				r = np.exp(-np.linalg.norm(info['tool_pos'] - target_pos) - np.log(2)) - 1
+				target_indices = np.nonzero(np.not_equal(info['target_string'], info['current_string']))
+				dist = min([np.linalg.norm(info['tool_pos'] - info['switch_pos'][index]) for index in target_indices])
+				r = np.exp(-self.reward_temp * dist + np.log(1 + self.reward_offset))
+				r -= np.sum(info['target_string'] != info['current_string'])
 		elif self.reward_type == 'sparse':
 			r = -1 + info['task_success']
 		elif self.reward_type == 'part_sparse':

image/rl/policies/encdec_policy.py
@@ -5,45 +5,54 @@ import torch.nn.functional as F
 import rlkit.torch.pytorch_util as ptu
 from rlkit.torch.distributions import OneHotCategorical as TorchOneHot
 
+
 class EncDecPolicy(PyTorchModule):
-	def __init__(self,  qf, features_keys, encoder=None, logit_scale=-1, eps=0, incl_state=True):
-		super().__init__()
-		self.encoder = encoder
-		self.qf = qf
-		self.features_keys = features_keys
-		self.logit_scale = logit_scale
-		self.eps = eps
-		self.incl_state = incl_state
+    def __init__(self, qf, features_keys, encoder=None, logit_scale=-1, eps=0, incl_state=True, sample=False,
+                 latent_size=None):
+        super().__init__()
+        self.encoder = encoder
+        self.qf = qf
+        self.features_keys = features_keys
+        self.logit_scale = logit_scale
+        self.eps = eps
+        self.incl_state = incl_state
+        self.sample = sample
+        self.latent_size = latent_size
+        if self.sample:
+            assert self.latent_size is not None
 
-	def get_action(self, obs):
-		features = [obs[k] for k in self.features_keys]
-		with th.no_grad():
-			raw_obs = obs['raw_obs']
-			if self.encoder != None:
-				if self.incl_state:
-					features.append(raw_obs)
-				pred_features = self.encoder.sample(th.Tensor(np.concatenate(features)).to(ptu.device)).detach()
-				obs['latents'] = pred_features.cpu().numpy()
-			else:
-				pred_features = np.concatenate(features)
-				obs['latents'] = pred_features
+    def get_action(self, obs):
+        features = [obs[k] for k in self.features_keys]
+        with th.no_grad():
+            raw_obs = obs['raw_obs']
+            if self.encoder != None:
+                if self.incl_state:
+                    features.append(raw_obs)
+                encoder_input = th.Tensor(np.concatenate(features)).to(ptu.device)
+                eps = th.normal(ptu.zeros(self.latent_size), 1) if self.sample else None
 
-			q_values, ainfo = self.qf.get_action(raw_obs,pred_features[:3])
-			q_values = ptu.tensor(q_values)
-			if np.random.rand() > self.eps:
-				action = F.one_hot(q_values.argmax().long(), 6).flatten()
+                pred_features = self.encoder.sample(encoder_input, eps=eps).detach()
+                obs['latents'] = pred_features.cpu().numpy()
+            else:
+                pred_features = np.concatenate(features)
+                obs['latents'] = pred_features
 
-			else:
-				action = ptu.zeros(6)
-				action[np.random.randint(6)] = 1
+            q_values, ainfo = self.qf.get_action(raw_obs, pred_features[:3])
+            q_values = ptu.tensor(q_values)
+            if np.random.rand() > self.eps:
+                action = F.one_hot(q_values.argmax().long(), 6).flatten()
 
-			# if self.logit_scale != -1:
-			# 	action = TorchOneHot(logits=self.logit_scale * q_values).sample().flatten().detach()
-			# 	# action = TorchOneHot(logits=self.logit_scale*(q_values-q_values.mean())/(1e-8+q_values.std())).sample().flatten().detach()
-			# else:
-			# 	action = F.one_hot(q_values.argmax().long(),6).flatten()
+            else:
+                action = ptu.zeros(6)
+                action[np.random.randint(6)] = 1
 
-		return ptu.get_numpy(action), ainfo
+        # if self.logit_scale != -1:
+        # 	action = TorchOneHot(logits=self.logit_scale * q_values).sample().flatten().detach()
+        # 	# action = TorchOneHot(logits=self.logit_scale*(q_values-q_values.mean())/(1e-8+q_values.std())).sample().flatten().detach()
+        # else:
+        # 	action = F.one_hot(q_values.argmax().long(),6).flatten()
 
-	def reset(self):
-		pass
+        return ptu.get_numpy(action), ainfo
+
+    def reset(self):
+        pass

image/rl/scripts/ddqn_cql_experiment_s2-calibrate.py
@@ -1,7 +1,7 @@
 import rlkit.torch.pytorch_util as ptu
 from rlkit.torch.networks import ConcatMlpPolicy, VAE
-from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 from rl.misc.calibration_rl_algorithm import BatchRLAlgorithm as TorchCalibrationRLAlgorithm
+from rl.misc.calibration_rl_algorithm_awr import BatchRLAlgorithm as TorchCalibrationRLAlgorithmAWR
 
 from rl.policies import EncDecPolicy, CalibrationPolicy
 from rl.path_collectors import FullPathCollector
@@ -66,6 +66,8 @@ def experiment(variant):
         qf,
         list(env.feature_sizes.keys()),
         encoder=encoder,
+        sample=True,
+        latent_size=variant['latent_size'],
     )
     eval_path_collector = FullPathCollector(
         eval_env,
@@ -76,6 +78,8 @@ def experiment(variant):
         qf=qf,
         features_keys=list(env.feature_sizes.keys()),
         encoder=encoder,
+        sample=True,
+        latent_size=variant['latent_size'],
         **variant['expl_kwargs']
     )
     calibration_policy = CalibrationPolicy(
@@ -120,7 +124,11 @@ def experiment(variant):
         sample_base=0,
         latent_size=variant['latent_size']
     )
-    algorithm = TorchCalibrationRLAlgorithm(
+
+    alg_class = TorchCalibrationRLAlgorithmAWR if 'AWR' in variant['trainer_kwargs']['use_supervised'] else \
+        TorchCalibrationRLAlgorithm
+    alg_class = TorchCalibrationRLAlgorithm
+    algorithm = alg_class(
         trainer=trainer,
         exploration_env=env,
         evaluation_env=eval_env,
@@ -151,7 +159,7 @@ if __name__ == "__main__":
 
     path_length = 200
     variant = dict(
-        pretrain_path=f'{args.env_name}_params_s1_dense_encoder_offset.pkl',
+        pretrain_path=f'{args.env_name}_params_s1_dense_encoder_5.pkl',
         latent_size=3,
         layer_size=64,
         expl_kwargs=dict(
@@ -167,8 +175,8 @@ if __name__ == "__main__":
             add_ood_term=-1,
             temp=10,
             min_q_weight=0,
-            sample=False,
-            beta=0,
+            sample=True,
+            beta=0.01,
             pos_weight=1
         ),
         algorithm_args=dict(
@@ -179,16 +187,18 @@ if __name__ == "__main__":
             num_expl_steps_per_train_loop=1,
             num_train_loops_per_epoch=1,
             collect_new_paths=True,
-            num_trains_per_train_loop=0,
+            num_trains_per_train_loop=1,
             # min_num_steps_before_training=1000
-            trajs_per_index=3,
-            calibration_indices=[0, 2]
+            trajs_per_index=5,
+            calibration_indices=None,
+            pretrain_steps=500,
+            max_failures=10
         ),
 
         env_config=dict(
             env_name=args.env_name,
             step_limit=path_length,
-            env_kwargs=dict(success_dist=.03, frame_skip=5, debug=False),
+            env_kwargs=dict(success_dist=.03, frame_skip=5, debug=False, num_targets=5, target_indices=[0, 2, 4]),
 
             action_type='disc_traj',
             smooth_alpha=.8,
@@ -198,7 +208,9 @@ if __name__ == "__main__":
             gaze_dim=128,
             state_type=0,
             reward_max=0,
-            reward_min=-1,
+            reward_min=-5,
+            reward_temp=2,
+            reward_offset=-0.1,
             reward_type='sparse',
             gaze_path=f'{args.env_name}_gaze_data_train.h5',
             eval_gaze_path=f'{args.env_name}_gaze_data_eval.h5'
@@ -207,7 +219,7 @@ if __name__ == "__main__":
     variants = []
 
     search_space = {
-        'seedid': [2000, 2001],
+        'seedid': [2000, 2001, 2002],
         'layer_norm': [True],
         'trainer_kwargs.soft_target_tau': [1e-2],
         'freeze_decoder': [True],

image/rl/scripts/ddqn_vib_pretrain.py
@@ -2,10 +2,9 @@ import rlkit.torch.pytorch_util as ptu
 from rlkit.torch.networks import ConcatMlpPolicy, VAE
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
 
-from rl.policies import EncDecPolicy
+from rl.policies import EncDecPolicy, KeyboardPolicy
 from rl.path_collectors import FullPathCollector
 from rl.misc.env_wrapper import default_overhead
-from rl.misc.simple_path_loader import SimplePathLoader
 from rl.trainers import EncDecCQLTrainer
 from rl.replay_buffers import HERReplayBuffer, ModdedReplayBuffer
 from rl.scripts.run_util import run_exp
@@ -72,7 +71,8 @@ def experiment(variant):
         qf,
         list(env.feature_sizes.keys()),
         encoder=encoder,
-        incl_state=False
+        incl_state=False,
+        sample=False,
     )
     eval_path_collector = FullPathCollector(
         eval_env,
@@ -85,7 +85,8 @@ def experiment(variant):
         encoder=encoder,
         logit_scale=variant['expl_kwargs']['logit_scale'],
         eps=variant['expl_kwargs']['eps'],
-        incl_state=False
+        incl_state=False,
+        sample=False,
     )
     expl_path_collector = FullPathCollector(
         env,
@@ -142,7 +143,7 @@ if __name__ == "__main__":
 
     path_length = 200
     variant = dict(
-        pretrain_path=f'{args.env_name}_params_s1_dense_encoder_offset.pkl',
+        pretrain_path=f'{args.env_name}_params_s1_dense_encoder_5.pkl',
         latent_size=3,
         layer_size=128,
         # her_k=4,
@@ -162,14 +163,25 @@ if __name__ == "__main__":
         algorithm_args=dict(
             batch_size=256,
             max_path_length=path_length,
-            num_epochs=100,
+            num_epochs=200,
             num_eval_steps_per_epoch=1000,
             num_expl_steps_per_train_loop=500,
             num_train_loops_per_epoch=10,
             collect_new_paths=True,
             num_trains_per_train_loop=100,
-            min_num_steps_before_training=1000
+            min_num_steps_before_training=10000
         ),
+        # algorithm_args=dict(
+        #     batch_size=256,
+        #     max_path_length=path_length,
+        #     num_epochs=10,
+        #     num_eval_steps_per_epoch=5000,
+        #     num_expl_steps_per_train_loop=0,
+        #     num_train_loops_per_epoch=1,
+        #     collect_new_paths=False,
+        #     num_trains_per_train_loop=0,
+        #     min_num_steps_before_training=1
+        # ),
 
         # demo_paths=[
         #     os.path.join(main_dir, "demos", f"{args.env_name}_model_on_policy_5000_debug.npy"),
@@ -178,7 +190,7 @@ if __name__ == "__main__":
         env_config=dict(
             env_name=args.env_name,
             step_limit=path_length,
-            env_kwargs=dict(success_dist=.03, frame_skip=5, debug=False),
+            env_kwargs=dict(success_dist=.03, frame_skip=5, debug=False, num_targets=3),
             action_type='disc_traj',
             smooth_alpha=.8,
             factories=[],
@@ -186,19 +198,21 @@ if __name__ == "__main__":
             gaze_dim=128,
             state_type=0,
             reward_max=0,
-            reward_min=-1,
+            reward_min=-3,
             reward_type='custom_switch',
+            reward_temp=2,
+            reward_offset=-0.1
         )
     )
     search_space = {
         'seedid': [2000],
-        'from_pretrain': [True],
+        'from_pretrain': [False],
         'layer_norm': [True],
         'expl_kwargs.logit_scale': [10],
         'expl_kwargs.eps': [0.1],
         'trainer_kwargs.soft_target_tau': [1e-2],
         # 'demo_path_proportions': [[int(5e3)], ],
-        'trainer_kwargs.beta': [.1],
+        'trainer_kwargs.beta': [.01],
         'buffer_type': [ModdedReplayBuffer],
         'replay_buffer_size': [200000],
     }

image/rl/trainers/enc_dec_cql_trainer_s1.py
@@ -45,9 +45,6 @@ class EncDecCQLTrainer(DQNTrainer):
 
         loss = 0
 
-        """
-        Supervised loss
-        """
         eps = th.normal(ptu.zeros((obs.size(0), self.latent_size)), 1) if self.sample else None
 
         if self.encoder is not None:
@@ -97,7 +94,9 @@ class EncDecCQLTrainer(DQNTrainer):
         # actions is a one-hot vector
         curr_qf = self.qf(*curr_obs_features)
         y_pred = th.sum(curr_qf * actions, dim=1, keepdim=True)
-        loss += self.qf_criterion(y_pred, y_target)
+        qf_loss = self.qf_criterion(y_pred, y_target)
+        loss += qf_loss
+
         """CQL term"""
         min_qf_loss = th.logsumexp(curr_qf / self.temp, dim=1, ).mean() * self.temp
         min_qf_loss = min_qf_loss - y_pred.mean()
@@ -125,8 +124,9 @@ class EncDecCQLTrainer(DQNTrainer):
         """
         if self._need_to_update_eval_statistics:
             self._need_to_update_eval_statistics = False
-            self.eval_statistics['QF Loss'] = np.mean(ptu.get_numpy(loss))
-            self.eval_statistics['QF OOD Loss'] = np.mean(ptu.get_numpy(min_qf_loss))
+            self.eval_statistics['Loss'] = np.mean(ptu.get_numpy(loss))
+            self.eval_statistics['QF Loss'] = np.mean(ptu.get_numpy(qf_loss))
+            self.eval_statistics['KL Loss'] = np.mean(ptu.get_numpy(kl_loss))
             self.eval_statistics['RF Accuracy'] = np.mean(ptu.get_numpy(accuracy))
             self.eval_statistics.update(create_stats_ordered_dict(
                 'Q Predictions',

image/rl/trainers/enc_dec_cql_trainer_s2_latent.py
@@ -61,20 +61,26 @@ class EncDecCQLTrainer(DQNTrainer):
             surrogate_probs = th.exp(-(th.norm(pred_latent - latents, dim=-1) ** 2))
             supervised_loss = th.nn.BCELoss(weight=weight)(surrogate_probs, success_indices)
         else:
+            if self.prev_encoder is not None:
+                target_latent = self.prev_encoder.sample(goals, eps=None)
+            else:
+                target_latent = goals
             if 'kl' in self.use_supervised:
-                if self.prev_encoder is not None:
-                    target_latent = self.prev_encoder.sample(goals, eps=None)
-                else:
-                    target_latent = goals
-                breakpoint()
                 target_q_dist = th.log_softmax(self.qf(obs, target_latent) * self.temp, dim=1).detach()
                 pred_q_dist = th.log_softmax(self.qf(obs, pred_latent) * self.temp,
                                              dim=1)
                 supervised_loss = th.nn.KLDivLoss(log_target=True)(pred_q_dist, target_q_dist)
+            elif 'AWR' in self.use_supervised:
+                pred_mean, pred_logvar = self.encoder.encode(inputs)
+                kl_loss = self.encoder.kl_loss(pred_mean, pred_logvar)
+                supervised_loss = th.nn.GaussianNLLLoss(reduction='none')(pred_mean, latents, th.exp(pred_logvar))
+                weights = th.where(success_indices, 1., 0.)
+                supervised_loss = th.mean(supervised_loss * weights)
             else:
                 supervised_loss = th.nn.MSELoss()(pred_latent[success_indices], latents[success_indices])
 
         loss += supervised_loss + self.beta * kl_loss
+
         """
         Update Q networks
         """
@@ -96,6 +102,9 @@ class EncDecCQLTrainer(DQNTrainer):
         if self._need_to_update_eval_statistics:
             self._need_to_update_eval_statistics = False
             self.eval_statistics['Loss'] = np.mean(ptu.get_numpy(loss))
+            self.eval_statistics['SL Loss'] = np.mean(ptu.get_numpy(supervised_loss))
+            self.eval_statistics['KL Loss'] = np.mean(ptu.get_numpy(kl_loss))
+
 
     @property
     def networks(self):

rlkit/rlkit/torch/networks/mlp.py
@@ -560,7 +560,7 @@ class VAE(PyTorchModule):
         return reconstruct_loss, self.kl_loss(mean, logvar)
 
     def kl_loss(self, mean, logvar):
-        return torch.mean(-0.5 * (1 + logvar - torch.square(mean) - torch.exp(logvar)))
+        return torch.mean(-0.5 * torch.sum(1 + logvar - torch.square(mean) - torch.exp(logvar), dim=-1))
 
     def encode(self, x):
         mean, logvar = torch.split(self.encoder(x), self.latent_size, dim=-1)