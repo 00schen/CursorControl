1dbe8d3c4e1a39841f2537e2a64b399503cba42c
Jensen <jensen.gao1999@gmail.com>
2021-06-25 19:42:01 -0700

    fix import

--- image/rl/policies/__init__.py
+++ image/rl/policies/__init__.py
@@ -1,4 +1,3 @@
 from .demonstration_policy import DemonstrationPolicy
-from .follower_policy import FollowerPolicy
 from .calibration_policy import CalibrationPolicy
 from .encdec_policy import EncDecPolicy, EncDecMakeDeterministic
